5e0e6c359e18d04e80f0f8982e6d723e476dedee
Jian Liu <onanying@126.com>
2016-12-02 14:38:13 +0800

    增加多个产品支持，两个模式可切换

--- Auth.php
+++ Auth.php
@@ -1,18 +1,30 @@
 <?php
 
 /**
- * 基于token的多平台身份认证类
+ * 基于token的一套账户体系下多个产品/多种平台/多种运行模式的身份认证类
  * @author 刘健 <59208859@qq.com>
  */
 class Auth
 {
 
-    private $db = '';
-    private $dbConf = ['host' => '192.168.0.68', 'port' => '6379', 'auth' => '123456'];
+    private $db = ''; // 数据库对象
 
-    public function __construct()
+    private $dbConf = ['host' => '127.0.0.1', 'port' => '6379', 'auth' => '']; // 数据库配置
+
+    const CONSTANT = 0; // 不变Token模式(有效期内)，同一个账号支持多个相同设备同时访问
+
+    const VARIATIONAL = 1; // 变化Token模式(有效期内)，同一个账号在多个相同设备只允许最后登录的设备访问
+
+    public function __construct($conf = null)
     {
+        empty($conf) or $this->configDB($conf);
         $this->connectDB();
+    }
+
+    // 配置数据库
+    public function configDB($conf)
+    {
+        $this->dbConf = $conf;
     }
 
     // 连接数据库
@@ -23,44 +35,34 @@ class Auth
         if ($state == false) {
             die('redis connect failure');
         }
-        if (!is_null($this->dbConf['auth'])) {
+        if (!empty($this->dbConf['auth'])) {
             $this->db->auth($this->dbConf['auth']);
         }
     }
 
     // 移动端Token
-    public function mobileToken($uid, $userdata, $expires = 2592000)
+    public function mobileToken($params)
     {
-        $accessToken = $this->store('mobile', $uid, $userdata, $expires);
-        return (object) ['accessToken' => $accessToken, 'expires' => $expires];
+        return $this->switchToken('mobile', 604800, $params);
     }
 
     // 浏览器端Token
-    public function browserToken($uid, $userdata, $expires = 172800)
+    public function browserToken($params)
     {
-        $accessToken = $this->store('browser', $uid, $userdata, $expires);
-        return (object) ['accessToken' => $accessToken, 'expires' => $expires];
+        return $this->switchToken('browser', 172800, $params);
     }
 
     // API应用Token
-    public function apiToken($uid, $userdata, $expires = 7200)
+    public function apiToken($params)
     {
-        $accessToken = $this->store('api', $uid, $userdata, $expires);
-        return (object) ['accessToken' => $accessToken, 'expires' => $expires];
+        return $this->switchToken('api', 7200, $params);
     }
 
-    // PC端给移动端授权Token
-    public function pamToken($uid, $userdata, $expires = 300)
+    // 登陆授权Token
+    public function loginToken($params)
     {
-        $accessToken = $this->store('pam', $uid, $userdata, $expires);
-        return (object) ['accessToken' => $accessToken, 'expires' => $expires];
-    }
-
-    // 移动端给PC端授权Token
-    public function mapToken($uid, $userdata, $expires = 300)
-    {
-        $accessToken = $this->store('map', $uid, $userdata, $expires);
-        return (object) ['accessToken' => $accessToken, 'expires' => $expires];
+        $params['runMode'] = self::VARIATIONAL; // 强制使用变化Token模式
+        return $this->switchToken('login', 300, $params);
     }
 
     // 展示token数据
@@ -69,55 +71,117 @@ class Auth
         // 解码access_token
         $tokenData = self::accessTokenDecode($accessToken);
         if ($tokenData === false) {
-            return null;
+            // token解码失败
+            return (object) ['errorCode' => 2];
         }
         // 取出数据
-        $key = "auth:{$tokenData['tokenType']}:{$tokenData['uid']}";
-        $userdata = $this->getData($key);
-        if (empty($userdata)) {
-            return null;
+        $key = "auth:{$tokenData['productName']}:{$tokenData['tokenType']}:{$tokenData['uid']}";
+        $userData = $this->getData($key);
+        if (empty($userData)) {
+            // token不存在
+            return (object) ['errorCode' => 1];
         }
         // 判断是否合法
-        if ($userdata['__secret__'] != $tokenData['secret']) {
-            return null;
+        if ($userData['__secret__'] != $tokenData['secret']) {
+            // token验证失败
+            return (object) ['errorCode' => 2];
         }
         // 剔除秘钥
-        unset($userdata['__secret__']);
+        unset($userData['__secret__']);
         // 返回
-        return (object) $userdata;
+        return (object) ['errorCode' => 0, 'userData' => (object) $userData];
+    }
+
+    // 切换token类型
+    private function switchToken($tokenType, $defaultExpires, $params)
+    {
+        if (!isset($params['runMode']) || !isset($params['productName']) || !isset($params['uid']) || !isset($params['userData'])) {
+            die('not found params keys [productName,uid,userData,runMode]');
+        }
+        $params['expires'] = isset($params['expires']) ? $params['expires'] : $defaultExpires;
+        switch ($params['runMode']) {
+            case self::CONSTANT:
+                return $this->constantToken($params['productName'], $tokenType, $params['uid'], $params['userData'], $params['expires']);
+                break;
+            case self::VARIATIONAL:
+                return $this->variationalToken($params['productName'], $tokenType, $params['uid'], $params['userData'], $params['expires']);
+                break;
+        }
+    }
+
+    // 有效期内不变的Token
+    private function constantToken($productName, $tokenType, $uid, $userData, $expires)
+    {
+        $safetyTime = 1;
+        $expires += $safetyTime;
+        $key = "auth:{$productName}:{$tokenType}:{$uid}";
+        $cacheExpires = $this->getDataExpires($key);
+        if ($cacheExpires >= $safetyTime) {
+            // 只修改用户数据
+            $this->edit($productName, $tokenType, $uid, $userData);
+            $cacheUserdata = $this->getData($key);
+            $accessToken = self::accessTokenEncode($productName, $tokenType, $uid, $cacheUserdata['__secret__']);
+            return (object) ['accessToken' => $accessToken, 'expires' => $cacheExpires - $safetyTime];
+        } else {
+            // 修改全部数据
+            $accessToken = $this->store($productName, $tokenType, $uid, $userData, $expires);
+            return (object) ['accessToken' => $accessToken, 'expires' => $expires - $safetyTime];
+        }
+    }
+
+    // 有效期内会变化的Token
+    private function variationalToken($productName, $tokenType, $uid, $userData, $expires)
+    {
+        $accessToken = $this->store($productName, $tokenType, $uid, $userData, $expires);
+        return (object) ['accessToken' => $accessToken, 'expires' => $expires];
     }
 
     // 存储token数据
-    private function store($tokenType, $uid, $userdata, $expires)
+    private function store($productName, $tokenType, $uid, $userData, $expires)
     {
         $secret = sha1($uid . time());
         // 增加secret至用户信息
-        $userdata = (array) $userdata;
-        $userdata['__secret__'] = $secret;
+        $userData = (array) $userData;
+        $userData['__secret__'] = $secret;
         // 保存数据
-        $key = "auth:{$tokenType}:{$uid}";
-        $this->setData($key, $userdata, $expires);
+        $key = "auth:{$productName}:{$tokenType}:{$uid}";
+        $this->setData($key, $userData, $expires);
         // 返回access_token
-        return self::accessTokenEncode($tokenType, $uid, $secret);
+        return self::accessTokenEncode($productName, $tokenType, $uid, $secret);
+    }
+
+    // 修改token数据
+    private function edit($productName, $tokenType, $uid, $userData)
+    {
+        $userData = (array) $userData;
+        // 保存数据
+        $key = "auth:{$productName}:{$tokenType}:{$uid}";
+        $this->setData($key, $userData, null);
     }
 
     // 保存数据
-    private function setData($key, $userdata, $expires)
+    private function setData($key, $userData, $expires)
     {
-        $this->db->hMset($key, $userdata);
-        $this->db->setTimeout($key, $expires);
+        $this->db->hMset($key, $userData);
+        is_null($expires) or $this->db->setTimeout($key, $expires);
     }
 
     // 取出数据
-    private function getData($uid)
+    private function getData($key)
     {
-        return $this->db->hGetAll($uid);
+        return $this->db->hGetAll($key);
+    }
+
+    // 获取数据的有效期
+    private function getDataExpires($key)
+    {
+        return $this->db->ttl($key);
     }
 
     // access_token编码
-    private static function accessTokenEncode($tokenType, $uid, $secret)
+    private static function accessTokenEncode($productName, $tokenType, $uid, $secret)
     {
-        return self::base64UrlEncode("$tokenType,$uid,$secret");
+        return self::base64UrlEncode("$productName,$tokenType,$uid,$secret");
     }
 
     // access_token解码
@@ -125,10 +189,10 @@ class Auth
     {
         $accessToken = self::base64UrlDecode($accessToken);
         $ary = explode(',', $accessToken);
-        if (count($ary) != 3) {
+        if (count($ary) != 4) {
             return false;
         }
-        return ['tokenType' => $ary[0], 'uid' => $ary[1], 'secret' => $ary[2]];
+        return ['productName' => $ary[0], 'tokenType' => $ary[1], 'uid' => $ary[2], 'secret' => $ary[3]];
     }
 
     // base64url编码
--- README.md
+++ README.md
@@ -1,50 +1,140 @@
 ## AuthToken
 
-在账号体系的信息系统中，对身份的鉴定是非常重要的事情，随着移动互联网时代到来，客户端的类型越来越多， 逐渐出现了一个服务器，N个客户端的格局，不同的客户端产生了不同的用户使用场景，本类根据不同的用户使用场景解决身份认证问题。
+基于token的一套账户体系下多个产品/多种平台/多种运行模式的身份认证类
 
 ### 架构设计
 
 [基于token的多平台身份认证架构设计](http://www.cnblogs.com/beer/p/6029861.html)
 
-### 细节说明
+### 功能简介
 
-- 每次新生成access_token时，该用户的该类型的旧access_token将失效
-- 在redis里，每个用户仅有一个hash，省空间、好同步，实现原理请研究源代码
+- 支持一个账号体系下多个产品
+- 每个产品都支持4种终端类型的token
+- 有3种类型的token支持两种运行模式
+- 支持 踢出其他相同终端 与 可多个相同终端同时在线 两种运行模式
 
-### 范例 (Example)
+### 说明文档
 
-生成移动端access_token，存储用户信息
+#### 存储用户信息，并生成移动端access_token
+
+productName：你的产品名称  
+uid: 用户id  
+userData: 需要存储的用户数据  
+runMode: 有 Auth::CONSTANT 、Auth::VARIATIONAL 两个参数 两种运行模式，后面会详细说明
+
+范例代码
 
 ```php
-$userdata = ['uid'=>1008,'name'=>'小花'];
+$userData = ['uid' => 1008, 'name' => '小花'];
 $auth = new Auth();
-$auth = $auth->mobileToken($userdata['uid'], $userdata); // uid建议使用类似微信的openid，而不使用数据库的原始uid
-echo $auth->accessToken;
-echo $auth->expires; // 默认有效期30天
+$params = ['productName' => 'product1', 'uid' => $userData['uid'], 'userData' => $userData, 'runMode' => Auth::CONSTANT];
+$auth = $auth->mobileToken($params); // uid建议使用类似微信的openid，而不使用数据库的原始uid
+var_dump($auth);
 ```
 
-通过access_token获取用户信息
+返回结果
+
+	object(stdClass)#3 (2) {
+	  ["accessToken"]=>
+	  string(82) "cHJvZHVjdDEsbW9iaWxlLDEwMDgsMmM5NTUyYzBlOGJmZGQ4MDlhZjI1ZmU5MzQ3MWQ5NDJhODk4YWFkOQ"
+	  ["expires"]=>
+	  int(600668)
+	}
+
+#### access_token 的4种终端类型
+
+**1. 移动端Token**
+
+用于手机app
+
+```php
+// 移动端Token，默认有效期7天
+$params = ['productName' => 'product1', 'uid' => $userData['uid'], 'userData' => $userData, 'runMode' => Auth::VARIATIONAL];
+$auth = $auth->mobileToken($params); 
+```
+
+**2. 浏览器端Token**
+
+用于PC或移动端的H5应用
+
+```php
+// 浏览器端Token，默认有效期2天
+$params = ['productName' => 'product1', 'uid' => $userData['uid'], 'userData' => $userData, 'runMode' => Auth::CONSTANT];
+$auth = $auth->browserToken($params); 
+```
+
+**3. API应用Token**
+
+用于PC端应用软件
+
+```php
+// API应用Token，默认有效期2小时
+$params = ['productName' => 'product1', 'uid' => $userData['uid'], 'userData' => $userData, 'runMode' => Auth::CONSTANT];
+$auth = $auth->apiToken($params); 
+```
+
+**4. 登陆授权Token**
+
+用于某个端通过保存在本地的uid,password或第三方平台返回的openid/unionid获取用户信息，然后通过用户信息生成一个token，再通过url或二维码的形式传递给另一个端，另一端接受后取出用户信息完成登录动作，等类似场景
+
+```php
+// 登陆授权Token，默认有效期5分钟，注意不需传runMode参数
+$params = ['productName' => 'product1', 'uid' => $userData['uid'], 'userData' => $userData];
+$auth = $auth->loginToken($params);
+```
+
+#### 通过access_token获取用户数据
+
+当access_token效验成功时返回用户数据，出现错误时会返回两种状态：   
+- token不存在：表示token可能超时了，重新获取即可   
+- token身份验证失败：表明该token为非法请求，或该用户在其他相同终端有登录
+
+范例代码
 
 ```php
 $accessToken = $_GET['access_token'];
 $auth = new Auth();
-$userdata = $auth->show($accessToken);
-if(is_null($userdata)){
-    die('access_token Authentication Failed');
+$result = $auth->show($accessToken);
+if ($result->errorCode == 1) {
+    die('access_token not exist');
 }
-echo $userdata->uid;
-echo $userdata->name;
+if ($result->errorCode == 2) {
+    die('access_token auth failed');
+}
+var_dump($result->userData);
 ```
 
-生成其它类型的access_token
+返回结果
 
-```php
-// 浏览器端Token，默认有效期2天
-$auth = $auth->browserToken($userdata['uid'], $userdata);
-// API应用Token，默认有效期2小时
-$auth = $auth->apiToken($userdata['uid'], $userdata);
-// PC端给移动端授权Token，默认有效期5分钟
-$auth = $auth->pamToken($userdata['uid'], $userdata);
-// 移动端给PC端授权Token，默认有效期5分钟
-$auth = $auth->mapToken($userdata['uid'], $userdata);
-```
+	object(stdClass)#3 (2) {
+	  ["uid"]=>
+	  string(4) "1008"
+	  ["name"]=>
+	  string(6) "小花"
+	}
+
+#### 踢出 与 可同时登录 两种运行模式
+
+runMode: 有 Auth::CONSTANT 、Auth::VARIATIONAL 两个参数
+Auth::VARIATIONAL 为 踢出模式  
+Auth::CONSTANT 为 可同时登录模式
+
+**1. 踢出模式**
+
+踢出模式每获取一次token，会生成一个新的access_token，有效期也重新开始，且旧的access_token失效。
+
+> 像QQ一样，同一账号在另一个手机登录时，本手机会提示该账号已经在其他设备登录，但是登录PC端却不会，因为不是同一类型终端。
+
+如何实现呢？
+
+> 因为access_token本身是会过期的，当 $auth->show($accessToken) 返回errorCode等于1时，说明access_token只是过期了，没有其他人登录，通知app重新获取新的access_token即可；当返回errorCode等于2时，说明token被其他用户生成了新的token，也就是说该账号在其他同类型终端登录了，现在要通知app把该用户退出至登陆页，并做提示。
+
+**2. 可同时登录模式**
+
+可同时登录模式每获取一次token，当该用户的token还未过期时，不会生成新的token，有效期也不会重新开始，只有在token过期后才会生成新的token。
+
+> 像QQ邮箱一样，同一账号可在多个电脑上登录，并不会把其他电脑上的账号强制退出。
+
+如何实现呢？
+
+> 因为要支持多个相同终端同时登录，所以 $auth->show($accessToken) 返回errorCode等于1或2时，都代表了该token失效了，通知app重新获取一次token即可，因为有效期内token不会变，最终多个相同终端的token会变成同一个token，当然也都一样同时过期。
